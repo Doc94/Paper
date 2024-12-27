e23f68c2c64c749ef69c838965f5dc4b1519bf25
Doc <nachito94@msn.com>
2024-12-27 15:21:36 -0300

    Improvement Explosion events
    - Handle KEEP explosion behaviour (gamerule or explosion by Wind Burst)
    - Call custom event damager for allow CraftBukkit know who make the explosion by Wind Burst

--- test-plugin/src/main/java/io/papermc/testplugin/TestPlugin.java
+++ test-plugin/src/main/java/io/papermc/testplugin/TestPlugin.java
@@ -1,6 +1,15 @@
 package io.papermc.testplugin;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockExplodeEvent;
+import org.bukkit.event.entity.EntityDamageByBlockEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TestPlugin extends JavaPlugin implements Listener {
@@ -10,6 +19,28 @@ public final class TestPlugin extends JavaPlugin implements Listener {
         this.getServer().getPluginManager().registerEvents(this, this);
 
         // io.papermc.testplugin.brigtests.Registration.registerViaOnEnable(this);
+    }
+
+    @EventHandler
+    public void onEntityExplodeEvent(EntityExplodeEvent event) {
+        Bukkit.broadcastMessage(ToStringBuilder.reflectionToString(event));
+        event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onBlockExplodeEvent(BlockExplodeEvent event) {
+        Bukkit.broadcastMessage(ToStringBuilder.reflectionToString(event));
+        event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onBlockBreakEvent(EntityDamageEvent event) {
+        Bukkit.broadcastMessage(ToStringBuilder.reflectionToString(event));
+        if (event instanceof EntityDamageByEntityEvent entityDamageByEntityEvent) {
+            Bukkit.broadcastMessage("Damager: " + entityDamageByEntityEvent.getDamager());
+        } else if (event instanceof EntityDamageByBlockEvent entityDamageByBlockEvent) {
+            Bukkit.broadcastMessage("Damager: " + entityDamageByBlockEvent.getDamager());
+        }
     }
 
 }
